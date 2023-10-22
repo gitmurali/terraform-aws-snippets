3c1b5dfd06c69cee061ddf41d68aa51c84aba8b9
Murali ⚡️ <murali.nutalapatii@gmail.com>
2023-10-22 21:51:39 +0100

    update sg

--- 1. aws_tf_vpc_basics/datasources.tf
+++ 1. aws_tf_vpc_basics/datasources.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "MyEc2Instance" {
   ami                    = data.aws_ami.example.id
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.auth.id
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [aws_security_group.allow_sgs.id]
   subnet_id              = aws_subnet.main.id
   user_data              = file("${path.module}/userdata.tpl")
 
