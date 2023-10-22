84c91ec0114d7dee8878e4a92c73579400dd7f33
Murali ⚡️ <murali.nutalapatii@gmail.com>
2023-10-22 22:08:33 +0100

    update security group id

--- 1. aws_tf_vpc_basics/datasources.tf
+++ 1. aws_tf_vpc_basics/datasources.tf
@@ -12,11 +12,11 @@ resource "aws_instance" "MyEc2Instance" {
   ami                    = data.aws_ami.example.id
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.auth.id
-  vpc_security_group_ids = [aws_security_group.allow_sgs.id]
+  vpc_security_group_ids = ["${aws_security_group.allow_sgs.id}"]
   subnet_id              = aws_subnet.main.id
   user_data              = file("${path.module}/userdata.tpl")
 
-  root_block_device {
+  root_block_device { 
     volume_size = 10
   }
 
